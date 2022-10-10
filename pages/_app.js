1b42ee87facd6e07dfd5efa40e2954737e5dccd9
HOMERO CABRERA ARAQUE <homero.cabrera@telefonica.com>
2022-10-10 02:55:52 -0500

    Primer commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Layout />
+    <Container>
+        <Component {...pageProps} />
+    </Container>
+       
+    </>
+    
+
+  );
 }
 
 export default MyApp
